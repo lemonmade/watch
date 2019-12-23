38d1ca5ae46c010f18a3642070b1c9b5b9be0c4b
Chris Sauve <chrismsauve@gmail.com>
2019-12-23 12:25:39 -0500

    Add worker

--- app/App.tsx
+++ app/App.tsx
@@ -1,13 +1,20 @@
 import React, {useEffect} from 'react';
 
-import {Router} from '@quilted/quilt';
+import {Router, createWorkerFactory} from '@quilted/quilt';
 import {Rating} from '@lemon/zest';
 
 interface Props {}
 
+const createWorker = createWorkerFactory(() => import('./worker'));
+
 export function App(_props: Props) {
   useEffect(() => {
     (async () => {
+      const worker = createWorker();
+
+      // eslint-disable-next-line no-console
+      console.log(await worker.hello());
+
       const fetched = await fetch('http://localhost:8080/api', {
         method: 'POST',
         headers: {Accept: 'application/json'},
