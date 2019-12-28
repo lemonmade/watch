262ba9741f7e9e2d567dc594deba2e93f2779b1d
Chris Sauve <chrismsauve@gmail.com>
2019-12-27 20:59:13 -0500

    Add storybook plugin

---
+++ packages/zest/src/components/Rating/Rating.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Rating} from './Rating';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {title: 'Rating'};
+
+export const basic = () => <Rating />;
--- packages/zest/src/components/Rating/Rating.tsx
+++ packages/zest/src/components/Rating/Rating.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 interface Props {}
 
 export function Rating(_props: Props) {
-  return <div>Four stars!</div>;
+  const [value, setValue] = useState('Five');
+  return (
+    <div
+      onPointerEnter={() => setValue('One')}
+      onPointerLeave={() => setValue('Five')}
+    >
+      {value} stars!
+    </div>
+  );
 }
