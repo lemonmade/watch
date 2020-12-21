3eeeb5745cd91f285488c0489f2bb28b2bc3e2cb
Chris Sauve <chrismsauve@gmail.com>
2020-12-20 21:22:37 -0500

    Fix external links

--- app/components/Link.tsx
+++ app/components/Link.tsx
@@ -1,1 +1,11 @@
-export {Link} from '@quilted/quilt';
+import React, {ComponentProps} from 'react';
+import {Link as RouterLink} from '@quilted/quilt';
+
+export function Link({to, ...rest}: ComponentProps<typeof RouterLink>) {
+  if (typeof to === 'string' && /^https?:\/\//.test(to)) {
+    // eslint-disable-next-line jsx-a11y/anchor-has-content
+    return <a href={to} target="_blank" rel="noopener noreferrer" {...rest} />;
+  }
+
+  return <RouterLink to={to} {...rest} />;
+}
