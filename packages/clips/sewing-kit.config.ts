a823a25daddc80162479170d750b5ad983d48f66
Chris Sauve <chrismsauve@gmail.com>
2021-02-05 23:10:39 -0500

    Add a package change

--- packages/clips/sewing-kit.config.ts
+++ packages/clips/sewing-kit.config.ts
@@ -5,5 +5,7 @@ import {buildFlexibleOutputs} from '@sewing-kit/plugin-package-flexible-outputs'
 
 export default createPackage((pkg) => {
   pkg.entry({root: './src/index'});
+  pkg.entry({name: 'react', root: './src/react/index'});
+  pkg.entry({name: 'vue', root: './src/vue/index'});
   pkg.use(quiltPackage(), buildFlexibleOutputs());
 });
