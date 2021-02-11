ab0abaf9103819c34159067d8002de833f3b472d
Chris Sauve <chrismsauve@gmail.com>
2021-02-10 20:19:01 -0500

    Refreshable clips

--- app/components/Clip/Clip.tsx
+++ app/components/Clip/Clip.tsx
@@ -1,4 +1,4 @@
-import {useMemo, useEffect} from 'react';
+import {useMemo, useEffect, useState} from 'react';
 import type {IdentifierForRemoteComponent} from '@remote-ui/core';
 import {
   useWorker,
@@ -13,8 +13,10 @@ import type {
   ApiForExtensionPoint,
   AllowedComponentsForExtensionPoint,
 } from '@watching/clips';
-import {createWorkerFactory} from '@remote-ui/web-workers';
+import {createWorkerFactory, expose} from '@remote-ui/web-workers';
 import type {ClipsExtensionApiVersion} from 'graphql/types';
+
+import styles from './Clip.css';
 
 export const createSandbox = createWorkerFactory(() =>
   import(/* webpackChunkName: 'ExtensionSandbox' */ './sandbox'),
@@ -34,29 +36,80 @@ interface Props<T extends ExtensionPoint> {
   script: string;
   api: Omit<ApiForExtensionPoint<T>, 'extensionPoint' | 'version'>;
   components: ReactComponentsForRuntimeExtension<T>;
+  local?: boolean;
 }
 
 export function Clip<T extends ExtensionPoint>({
   extensionPoint,
   version,
   script,
+  ...rest
+}: Props<T>) {
+  const [key, refreshExtension] = useExtensionKey({
+    extensionPoint,
+    version,
+    script,
+  });
+
+  return (
+    <ClipInternal
+      key={key}
+      extensionPoint={extensionPoint}
+      version={version}
+      script={script}
+      {...rest}
+      onReload={refreshExtension}
+    />
+  );
+}
+
+function useExtensionKey<T extends ExtensionPoint>({
+  extensionPoint,
+  version,
+  script,
+}: Pick<Props<T>, 'extensionPoint' | 'version' | 'script'>): [
+  string,
+  () => void,
+] {
+  const [reloadId, setReloadId] = useState(0);
+
+  return [
+    `${extensionPoint}_${version}_${script}_${reloadId}`,
+    () => setReloadId((id) => id + 1),
+  ];
+}
+
+interface InternalProps<T extends ExtensionPoint> extends Props<T> {
+  onReload?(): void;
+}
+
+/* eslint-disable react-hooks/exhaustive-deps */
+function ClipInternal<T extends ExtensionPoint>({
+  extensionPoint,
+  version,
+  script,
   api,
   components,
-}: Props<T>) {
+  onReload,
+  local = false,
+}: InternalProps<T>) {
+  const sandbox = useWorker(createSandbox);
+
   const {controller, receiver} = useMemo(
     () => ({
       receiver: new RemoteReceiver(),
       controller: createController(components),
     }),
-    // eslint-disable-next-line no-warning-comments
-    // TODO: need to make this work properly if props change
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     [],
   );
 
-  const sandbox = useWorker(createSandbox);
-
   useEffect(() => {
+    expose(sandbox, {
+      reload() {
+        onReload?.();
+      },
+    });
+
     (async () => {
       await sandbox.load(script);
       await sandbox.render(
@@ -66,8 +119,19 @@ export function Clip<T extends ExtensionPoint>({
         {...api, version, extensionPoint} as any,
       );
     })();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [sandbox, extensionPoint]);
+  }, [sandbox]);
 
-  return <RemoteRenderer controller={controller} receiver={receiver} />;
+  const content = (
+    <RemoteRenderer controller={controller} receiver={receiver} />
+  );
+
+  return local ? (
+    <div className={styles.LocalClip}>
+      <p className={styles.LocalClipHeading}>Local extension</p>
+      {content}
+    </div>
+  ) : (
+    content
+  );
 }
+/* eslint-enable react-hooks/exhaustive-deps */
