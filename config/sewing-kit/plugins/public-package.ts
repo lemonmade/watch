7ab9a6313b71dd6c00a0fb2861f520785945fe49
Chris Sauve <chrismsauve@gmail.com>
2021-02-20 23:49:32 -0500

    Fix .d.ts copying

--- config/sewing-kit/plugins/public-package.ts
+++ config/sewing-kit/plugins/public-package.ts
@@ -1,4 +1,8 @@
-import {createComposedProjectPlugin, Package} from '@sewing-kit/plugins';
+import {
+  createComposedProjectPlugin,
+  createProjectBuildPlugin,
+  Package,
+} from '@sewing-kit/plugins';
 import {buildFlexibleOutputs} from '@sewing-kit/plugin-package-flexible-outputs';
 
 import {buildTypeScriptDefinitions} from './package-typescript';
@@ -7,5 +11,21 @@ export const publicPackage = (
   options?: Parameters<typeof buildFlexibleOutputs>[0],
 ) =>
   createComposedProjectPlugin<Package>('Watch.PublicPackage', (composer) => {
-    composer.use(buildFlexibleOutputs(options), buildTypeScriptDefinitions());
+    composer.use(
+      buildFlexibleOutputs(options),
+      createProjectBuildPlugin(
+        'Watch.PublicPackage.BabelDTSIgnores',
+        ({hooks}) => {
+          hooks.target.hook(({hooks}) => {
+            hooks.configure.hook((configuration) => {
+              configuration.babelIgnorePatterns?.hook((ignore) => [
+                ...ignore,
+                '**/*.d.ts',
+              ]);
+            });
+          });
+        },
+      ),
+      buildTypeScriptDefinitions(),
+    );
   });
