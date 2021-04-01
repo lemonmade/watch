7dc85e5b714b0245ffe73a3fb5467b0e6f9ebf7b
Chris Sauve <chrismsauve@gmail.com>
2021-03-31 20:21:51 -0400

    no-cache

--- app/foundation/Http/Http.tsx
+++ app/foundation/Http/Http.tsx
@@ -3,6 +3,7 @@ import {useResponseHeader} from '@quilted/quilt/http';
 export function Http() {
   useResponseHeader('Content-Type', 'text/html');
   useResponseHeader('X-Lemon', '1');
+  useResponseHeader('Cache-Control', 'no-cache');
 
   return null;
 }
