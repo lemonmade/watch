4df3fc839870ea7fb20329194e3d2dca48a92a12
Chris Sauve <chrismsauve@gmail.com>
2021-04-07 19:01:20 -0400

    Wild styling ideas

--- packages/basics/src/components/Layout/Layout.tsx
+++ packages/basics/src/components/Layout/Layout.tsx
@@ -18,6 +18,84 @@ export interface Media<T> {
   readonly viewport?: ViewportMedia;
 }
 
+// type PixelValue = `@px@${number}`;
+
+// function Pixels(num: number): PixelValue {
+//   return `@px@${num}` as any;
+// }
+
+// type PercentValue = `@%@${number}`;
+
+// function Percent(num: number): PercentValue {
+//   return `@%@${num}` as any;
+// }
+
+// type KeywordValue<T extends string = string> = `@n@${T}`;
+
+// function Keyword<T extends string>(value: T): KeywordValue<T> {
+//   return `@k@${value}` as any;
+// }
+
+// type UnknownValue<T extends string | number | boolean> = `@u@${T}`;
+
+// function Unknown<T extends string | number | boolean>(value: T): UnknownValue<T> {
+//   return `@u@${value}` as any;
+// }
+
+// type Unset = '';
+
+// type ViewportSizeCondition = `@v@${KeywordValue<ViewportSize> | Unset}_${KeywordValue<ViewportSize | Unset>}`;
+
+// function ViewportSize({min, max}: {
+//   readonly min?: ViewportSize;
+//   readonly max?: ViewportSize;
+// }): ViewportSizeCondition {
+//    return `@vc@${min ? Keyword(min) : ''}_${max ? Keyword(max) : ''}` as any;
+// }
+
+// type InteractionStateMode = 'hover' | 'active' | 'focused';
+// type InteractionStateCondition = `@i@${InteractionStateMode}`
+
+// function InteractionState(value: InteractionStateMode): InteractionStateCondition {
+//   return `@i@${value}` as any;
+// }
+
+// type DynamicValueAllowedConditions = ViewportSizeCondition | InteractionStateCondition;
+
+// type DynamicValue<Value extends string | number | boolean, Condition extends DynamicValueAllowedConditions> = `@dv@${Value & string}_${Condition}`;
+
+// function isStyleValue(value: unknown) {
+//   return typeof value === 'string' && value[0] === '@';
+// }
+
+// function DynamicValue<
+//   Value extends string | number | boolean,
+//   Condition extends DynamicValueAllowedConditions
+// >(
+//   value: Value,
+//   {when: condition}: {when: Condition},
+// ): DynamicValue<Value, Condition> {
+//   return `@dv@${
+//     isStyleValue(value) ? value : Unknown(value)
+//   }_${condition}` as any;
+// }
+
+// type ValueOrDynamicValueList<Value extends string | number | boolean> = Value | [Value | DynamicValue<Value, DynamicValueAllowedConditions>, ...DynamicValue<Value, DynamicValueAllowedConditions>[]];
+
+// type SizeKeyword = 'auto' | 'fill' | 'hidden';
+// type AllowedSize = SizeKeyword | false | number | KeywordValue<SizeKeyword> | PixelValue | PercentValue;
+
+// function parseType<ParsedType, AllowedType extends ParsedType>(type: AllowedType): ParsedType {
+
+// }
+
+// const sizes: ValueOrDynamicValueList<AllowedSize> = [
+//   'fill',
+//   DynamicValue(Pixels(300), {when: InteractionState('hover')}),
+//   DynamicValue(Pixels(123), {when: ViewportSize({min: 'medium'})}),
+//   DynamicValue(Percent(20), {when: ViewportSize({min: 'large'})}),
+// ];
+
 export type ValueOrMediaList<T> = T | Media<T>[];
 
 interface Props {
