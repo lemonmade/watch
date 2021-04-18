416c7fb82130623772cb846917e5f535c9e0c130
Chris Sauve <chrismsauve@gmail.com>
2021-04-17 22:56:18 -0400

    Fix type-check

--- packages/zest/src/components/index.ts
+++ packages/zest/src/components/index.ts
@@ -3,7 +3,6 @@ export {Button} from './Button';
 export {CollapseGroup} from './CollapseGroup';
 export {DateField} from './DateField';
 export {MediaGrid} from './MediaGrid';
-export {Page} from './Page';
 export {Poster} from './Poster';
 export {Rating} from './Rating';
 export {SpacingScale} from './SpacingScale';
