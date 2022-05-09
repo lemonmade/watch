91c3d2b344a0c5eac65ef025ad0e9d7c33c26ce5
Chris Sauve <chrismsauve@gmail.com>
2022-05-09 02:44:33 -0400

    Refactor test files

--- packages/graphql/src/server/live.test.ts
+++ packages/graphql/src/server/live.test.ts
@@ -46,175 +46,189 @@ interface Schema {
 
 /* eslint-enable @typescript-eslint/ban-types */
 
-it('works with static values', async () => {
-  const query = parse(`query { version }`);
+describe('execute()', () => {
+  it('returns static field values', async () => {
+    const query = parse(`query { version }`);
 
-  const resolver = createQueryResolver(() => ({
-    version: 'v1',
-  }));
+    const resolver = createQueryResolver(() => ({
+      version: 'v1',
+    }));
 
-  const result = await execute(query, resolver).untilDone();
+    const result = await execute(query, resolver).untilDone();
 
-  expect(result).toStrictEqual({version: 'v1'});
-});
+    expect(result).toStrictEqual({version: 'v1'});
+  });
 
-it('works with nullish values', async () => {
-  const query = parse(`query { me { partner } }`);
+  it('returns nullish field values', async () => {
+    const query = parse(`query { me { partner } }`);
 
-  const resolver = createQueryResolver(({object}) => ({
-    me: object('Person', {
-      name: 'Chris',
-      pets: [],
-    }),
-  }));
-
-  const result = await execute(query, resolver).untilDone();
-
-  expect(result).toStrictEqual({me: {partner: null}});
-});
-
-it('works with nested objects', async () => {
-  const query = parse(`query { me { partner { name } } }`);
-
-  const resolver = createQueryResolver(({object}) => ({
-    me: object('Person', {
-      name: 'Chris',
-      pets: [],
-      partner: object('Person', {
-        name: () => Promise.resolve('Mik'),
+    const resolver = createQueryResolver(({object}) => ({
+      me: object('Person', {
+        name: 'Chris',
         pets: [],
       }),
-    }),
-  }));
+    }));
 
-  const result = await execute(query, resolver).untilDone();
+    const result = await execute(query, resolver).untilDone();
 
-  expect(result).toStrictEqual({me: {partner: {name: 'Mik'}}});
-});
-
-it('works with lists', async () => {
-  const query = parse(`query { me { pets { name } } }`);
-
-  const resolver = createQueryResolver(({object}) => ({
-    me: object('Person', {
-      name: 'Chris',
-      pets: [object('Dog', {name: 'Winston'}), object('Dog', {name: 'Molly'})],
-    }),
-  }));
-
-  const result = await execute(query, resolver).untilDone();
-
-  expect(result).toStrictEqual({
-    me: {pets: [{name: 'Winston'}, {name: 'Molly'}]},
+    expect(result).toStrictEqual({me: {partner: null}});
   });
-});
 
-it('works with inline fragments on a concrete type', async () => {
-  const query = parse(`
-    query { me { pets { __typename ... on Dog { breed } } } }
-  `);
+  it('returns nested field selections', async () => {
+    const query = parse(`query { me { partner { name } } }`);
 
-  const resolver = createQueryResolver(({object}) => ({
-    me: object('Person', {
-      name: 'Chris',
-      pets: [
-        object('Dog', {name: 'Molly'}),
-        object('Dog', {name: 'Winston', breed: 'Black Lab'}),
-        object('Cat', {name: 'Luna'}),
-      ],
-    }),
-  }));
+    const resolver = createQueryResolver(({object}) => ({
+      me: object('Person', {
+        name: 'Chris',
+        pets: [],
+        partner: object('Person', {
+          name: () => Promise.resolve('Mik'),
+          pets: [],
+        }),
+      }),
+    }));
 
-  const result = await execute(query, resolver).untilDone();
+    const result = await execute(query, resolver).untilDone();
 
-  expect(result).toStrictEqual({
-    me: {
-      pets: [
-        {__typename: 'Dog', breed: null},
-        {__typename: 'Dog', breed: 'Black Lab'},
-        {__typename: 'Cat'},
-      ],
-    },
+    expect(result).toStrictEqual({me: {partner: {name: 'Mik'}}});
   });
-});
 
-it('works with promises', async () => {
-  const query = parse(`query { version }`);
+  it('returns field selections on lists', async () => {
+    const query = parse(`query { me { pets { name } } }`);
 
-  const resolver = createQueryResolver(() => ({
-    version: () => Promise.resolve('v1'),
-  }));
+    const resolver = createQueryResolver(({object}) => ({
+      me: object('Person', {
+        name: 'Chris',
+        pets: [
+          object('Dog', {name: 'Winston'}),
+          object('Dog', {name: 'Molly'}),
+        ],
+      }),
+    }));
 
-  const result = await execute(query, resolver).untilDone();
+    const result = await execute(query, resolver).untilDone();
 
-  expect(result).toStrictEqual({version: 'v1'});
-});
+    expect(result).toStrictEqual({
+      me: {pets: [{name: 'Winston'}, {name: 'Molly'}]},
+    });
+  });
 
-it('works with iterators', async () => {
-  const spy = jest.fn();
-  const query = parse(`query { version }`);
+  describe('inline fragments', () => {
+    it('returns inline fragment selections on concrete types', async () => {
+      const query = parse(`
+        query { me { pets { __typename ... on Dog { breed } } } }
+      `);
 
-  const resolver = createQueryResolver(() => ({
-    version: () => iterate(['v1', 'v2', 'v3']),
-  }));
-
-  for await (const result of execute(query, resolver)) {
-    spy(result);
-  }
-
-  expect(spy).toHaveBeenCalledTimes(3);
-  expect(spy).toHaveBeenLastCalledWith({version: `v3`});
-});
-
-it('works with iterators in nested objects', async () => {
-  const spy = jest.fn();
-  const query = parse(`query { me { name } }`);
-
-  const resolver = createQueryResolver(({object}) => ({
-    me: object('Person', {
-      name: () => iterate(['Chris 1', 'Chris 2', 'Chris 3']),
-      pets: [],
-    }),
-  }));
-
-  for await (const result of execute(query, resolver)) {
-    spy(result);
-  }
-
-  expect(spy).toHaveBeenCalledTimes(3);
-  expect(spy).toHaveBeenCalledWith({me: {name: `Chris 3`}});
-});
-
-it('works with nested iterators', async () => {
-  const spy = jest.fn();
-  const query = parse(`query { me { pets { name age } } }`);
-
-  const resolver = createQueryResolver(({object}) => ({
-    me: object('Person', {
-      name: 'Chris',
-      pets: () =>
-        iterate([
-          [],
-          [
+      const resolver = createQueryResolver(({object}) => ({
+        me: object('Person', {
+          name: 'Chris',
+          pets: [
             object('Dog', {name: 'Molly'}),
-            object('Dog', {name: 'Winston', age: () => iterate([8, 9, 10])}),
+            object('Dog', {name: 'Winston', breed: 'Black Lab'}),
+            object('Cat', {name: 'Luna'}),
           ],
-        ]),
-    }),
-  }));
+        }),
+      }));
 
-  for await (const result of execute(query, resolver)) {
-    spy(result);
-  }
+      const result = await execute(query, resolver).untilDone();
 
-  expect(spy).toHaveBeenCalledTimes(4);
-  expect(spy).toHaveBeenCalledWith({
-    me: {
-      pets: [
-        {name: 'Molly', age: null},
-        {name: 'Winston', age: 10},
-      ],
-    },
+      expect(result).toStrictEqual({
+        me: {
+          pets: [
+            {__typename: 'Dog', breed: null},
+            {__typename: 'Dog', breed: 'Black Lab'},
+            {__typename: 'Cat'},
+          ],
+        },
+      });
+    });
+  });
+
+  describe('promises', () => {
+    it('returns field values that return promises', async () => {
+      const query = parse(`query { version }`);
+
+      const resolver = createQueryResolver(() => ({
+        version: () => Promise.resolve('v1'),
+      }));
+
+      const result = await execute(query, resolver).untilDone();
+
+      expect(result).toStrictEqual({version: 'v1'});
+    });
+  });
+
+  describe('iterators', () => {
+    it('yields for field values that return iterators', async () => {
+      const spy = jest.fn();
+      const query = parse(`query { version }`);
+
+      const resolver = createQueryResolver(() => ({
+        version: () => iterate(['v1', 'v2', 'v3']),
+      }));
+
+      for await (const result of execute(query, resolver)) {
+        spy(result);
+      }
+
+      expect(spy).toHaveBeenCalledTimes(3);
+      expect(spy).toHaveBeenLastCalledWith({version: `v3`});
+    });
+
+    it('yields for iterators in nested selections', async () => {
+      const spy = jest.fn();
+      const query = parse(`query { me { name } }`);
+
+      const resolver = createQueryResolver(({object}) => ({
+        me: object('Person', {
+          name: () => iterate(['Chris 1', 'Chris 2', 'Chris 3']),
+          pets: [],
+        }),
+      }));
+
+      for await (const result of execute(query, resolver)) {
+        spy(result);
+      }
+
+      expect(spy).toHaveBeenCalledTimes(3);
+      expect(spy).toHaveBeenCalledWith({me: {name: `Chris 3`}});
+    });
+
+    it('yields for nested iterators', async () => {
+      const spy = jest.fn();
+      const query = parse(`query { me { pets { name age } } }`);
+
+      const resolver = createQueryResolver(({object}) => ({
+        me: object('Person', {
+          name: 'Chris',
+          pets: () =>
+            iterate([
+              [],
+              [
+                object('Dog', {name: 'Molly'}),
+                object('Dog', {
+                  name: 'Winston',
+                  age: () => iterate([8, 9, 10]),
+                }),
+              ],
+            ]),
+        }),
+      }));
+
+      for await (const result of execute(query, resolver)) {
+        spy(result);
+      }
+
+      expect(spy).toHaveBeenCalledTimes(4);
+      expect(spy).toHaveBeenCalledWith({
+        me: {
+          pets: [
+            {name: 'Molly', age: null},
+            {name: 'Winston', age: 10},
+          ],
+        },
+      });
+    });
   });
 });
 
