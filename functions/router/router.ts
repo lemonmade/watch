542ee197bdc05439744cdc412caea9ac5f0038a8
Chris Sauve <chrismsauve@gmail.com>
2022-06-19 18:50:31 -0400

    Move S3 buckets to Cloudflare

--- functions/router/router.ts
+++ functions/router/router.ts
@@ -1,23 +1,24 @@
 const APP_HOST = 'watch-test-app.fly.dev';
-const APP_ASSETS_HOST = 'watch-assets-app.s3.us-east-1.amazonaws.com';
-const CLIPS_ASSETS_HOST = 'watch-assets-clips.s3.us-east-1.amazonaws.com';
+
+interface CloudflareR2Bucket {
+  get(key: string): any;
+}
 
 // TODO: caching
-const handler: ExportedHandler = {
-  async fetch(request: Request) {
+const handler: ExportedHandler<{
+  APP_ASSETS: CloudflareR2Bucket;
+  CLIPS_ASSETS: CloudflareR2Bucket;
+}> = {
+  async fetch(request, env) {
     const url = new URL(request.url);
     const {pathname} = url;
 
     if (pathname.startsWith('/assets/app/')) {
-      return rewriteAndFetch(request, (url) => {
-        url.host = APP_ASSETS_HOST;
-      });
+      return assetFromBucket(url, env.APP_ASSETS);
     }
 
     if (pathname.startsWith('/assets/clips/')) {
-      return rewriteAndFetch(request, (url) => {
-        url.host = CLIPS_ASSETS_HOST;
-      });
+      return assetFromBucket(url, env.CLIPS_ASSETS);
     }
 
     return rewriteAndFetch(request, (url) => {
@@ -38,4 +39,21 @@ function rewriteAndFetch(request: Request, rewrite: (url: URL) => URL | void) {
   newRequest.headers.set('X-Forwarded-Host', originalUrl.host);
 
   return fetch(newRequest);
+}
+
+// @see https://developers.cloudflare.com/r2/get-started/
+async function assetFromBucket(url: URL, bucket: any) {
+  const object = await bucket.get(url.pathname.slice(1));
+
+  if (!object || !object.body) {
+    return new Response('Not Found', {status: 404});
+  }
+
+  const headers = new Headers();
+  object.writeHttpMetadata(headers);
+  headers.set('etag', object.httpEtag);
+
+  return new Response(object.body, {
+    headers,
+  });
 }
