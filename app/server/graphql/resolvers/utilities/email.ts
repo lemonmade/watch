4c41d6fb3d3d959d43b29ab35514c71d3d01b7f9
Chris Sauve <chrismsauve@gmail.com>
2022-06-19 17:36:09 -0400

    App GCP fixes

--- app/server/graphql/resolvers/utilities/email.ts
+++ app/server/graphql/resolvers/utilities/email.ts
@@ -3,7 +3,9 @@ import type {EmailType, PropsForEmail} from '../../../../../functions/email';
 
 declare module '@quilted/quilt/env' {
   interface EnvironmentVariables {
-    EMAIL_TOPIC: string;
+    GOOGLE_CLOUD_PROJECT_ID: string;
+    GOOGLE_CLOUD_CREDENTIALS: string;
+    GOOGLE_CLOUD_EMAIL_TOPIC: string;
   }
 }
 
@@ -14,9 +16,11 @@ export async function enqueueSendEmail<T extends EmailType>(
 ) {
   const {PubSub} = await import('@google-cloud/pubsub');
 
-  const pubsub = new PubSub();
+  const pubsub = new PubSub({
+    projectId: Env.GOOGLE_CLOUD_PROJECT_ID,
+  });
 
-  await pubsub.topic(Env.EMAIL_TOPIC).publishMessage({
+  await pubsub.topic(Env.GOOGLE_CLOUD_EMAIL_TOPIC).publishMessage({
     json: {
       type,
       props,
