7d33445bde9b2660d221e60499f2b5482d430840
Chris Sauve <chrismsauve@gmail.com>
2022-10-10 12:56:54 -0400

    Fix title and emoji for GraphiQL

--- app/server/graphql.ts
+++ app/server/graphql.ts
@@ -143,53 +143,64 @@ async function authenticate(
 }
 
 handler.get(() => {
-  return html(stripIndent`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <style>
-          body {
-            height: 100%;
-            margin: 0;
-            width: 100%;
-            overflow: hidden;
-          }
-    
-          #graphiql {
-            height: 100vh;
-          }
-        </style>
-    
-        <script
-          crossorigin
-          src="https://unpkg.com/react@17/umd/react.development.js"
-        ></script>
-        <script
-          crossorigin
-          src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"
-        ></script>
-    
-        <link rel="stylesheet" href="https://unpkg.com/graphiql/graphiql.min.css" />
-      </head>
-    
-      <body>
-        <div id="graphiql">Loading...</div>
-        <script
-          src="https://unpkg.com/graphiql/graphiql.min.js"
-          type="application/javascript"
-        ></script>
-        <script>
-          ReactDOM.render(
-            React.createElement(GraphiQL, {
-              fetcher: GraphiQL.createFetcher({
-                url: window.location.href,
+  return html(
+    stripIndent`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Watch • GraphQL</title>
+          <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>📺</text></svg>">
+          <meta charset="utf-8">
+
+          <style>
+            body {
+              height: 100%;
+              margin: 0;
+              width: 100%;
+              overflow: hidden;
+            }
+      
+            #graphiql {
+              height: 100vh;
+            }
+          </style>
+      
+          <script
+            crossorigin
+            src="https://unpkg.com/react@17/umd/react.development.js"
+          ></script>
+          <script
+            crossorigin
+            src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"
+          ></script>
+      
+          <link rel="stylesheet" href="https://unpkg.com/graphiql/graphiql.min.css" />
+        </head>
+      
+        <body>
+          <div id="graphiql">Loading...</div>
+          <script
+            src="https://unpkg.com/graphiql/graphiql.min.js"
+            type="application/javascript"
+          ></script>
+          <script>
+            ReactDOM.render(
+              React.createElement(GraphiQL, {
+                fetcher: GraphiQL.createFetcher({
+                  url: window.location.href,
+                }),
+                defaultEditorToolsVisibility: true,
               }),
-              defaultEditorToolsVisibility: true,
-            }),
-            document.getElementById('graphiql'),
-          );
-        </script>
-      </body>
-    </html>
-  `);
+              document.getElementById('graphiql'),
+            );
+          </script>
+        </body>
+      </html>
+    `,
+    {
+      headers: {
+        'Cache-Control': 'no-store',
+      },
+    },
+  );
 });
