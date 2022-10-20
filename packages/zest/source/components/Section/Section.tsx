22f96e3da1911f93f92af41d5438e434ed0194f1
Chris Sauve <chrismsauve@gmail.com>
2022-10-19 20:48:59 -0400

    Fix Section component

--- packages/zest/source/components/Section/Section.tsx
+++ packages/zest/source/components/Section/Section.tsx
@@ -1,39 +1,16 @@
-import type {PropsWithChildren} from 'react';
-
-import {
-  toHeadingLevel,
-  AutoHeadingContext,
-  useAutoHeadingLevel,
-} from '../../utilities/headings';
+import {type PropsWithChildren} from 'react';
+import {NestedHeadingLevel} from '../Heading';
 
 import {useViewProps, resolveViewProps, type ViewProps} from '../View';
 
-interface Props extends ViewProps {
-  content?: 'header' | 'footer';
-}
+interface Props extends ViewProps {}
 
-export function Section({
-  content,
-  children,
-  ...viewProps
-}: PropsWithChildren<Props>) {
+export function Section({children, ...viewProps}: PropsWithChildren<Props>) {
   const view = useViewProps(viewProps);
-  const currentLevel = useAutoHeadingLevel();
-
-  let level = currentLevel ?? 0;
-  let Element: 'section' | 'footer' | 'header' = 'section';
-
-  if (content === 'header') {
-    Element = 'header';
-  } else if (content === 'footer') {
-    Element = 'footer';
-  } else {
-    level += 1;
-  }
 
   return (
-    <AutoHeadingContext.Provider value={toHeadingLevel(level)}>
-      <Element {...resolveViewProps(view)}>{children}</Element>
-    </AutoHeadingContext.Provider>
+    <NestedHeadingLevel>
+      <section {...resolveViewProps(view)}>{children}</section>
+    </NestedHeadingLevel>
   );
 }
