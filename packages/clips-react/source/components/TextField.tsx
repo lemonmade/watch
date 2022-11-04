ba6469340f248c530bdb3daa8c86854c9e091b25
Chris Sauve <chrismsauve@gmail.com>
2022-11-03 22:49:01 -0400

    Add TextField React component

---
+++ packages/clips-react/source/components/TextField.tsx
@@ -0,0 +1,4 @@
+import {TextField as BaseTextField} from '@watching/clips';
+import {createRemoteReactComponent} from './shared';
+
+export const TextField = createRemoteReactComponent(BaseTextField);
--- packages/clips-react/source/components/index.ts
+++ packages/clips-react/source/components/index.ts
@@ -2,4 +2,5 @@ export {Action} from './Action';
 export {BlockStack} from './BlockStack';
 export {InlineStack} from './InlineStack';
 export {Text} from './Text';
+export {TextField} from './TextField';
 export {View} from './View';
