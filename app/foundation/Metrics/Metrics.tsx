8f20ed8d1db880485732db95ba606234c944ac06
Chris Sauve <chrismsauve@gmail.com>
2023-01-02 10:01:27 -0500

    Log metrics in dev and use sendBeacon

--- app/foundation/Metrics/Metrics.tsx
+++ app/foundation/Metrics/Metrics.tsx
@@ -2,18 +2,35 @@ import {
   usePerformanceNavigationEvent,
   type PropsWithChildren,
 } from '@quilted/quilt';
+import Env from '@quilted/quilt/env';
 
 export function Metrics({children}: PropsWithChildren) {
   usePerformanceNavigationEvent(async (navigation) => {
-    await fetch('/internal/metrics/navigation', {
-      method: 'POST',
-      body: JSON.stringify({
-        navigations: [navigation.toJSON()],
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
+    const data = navigation.toJSON();
+
+    if (Env.MODE === 'development') {
+      // eslint-disable-next-line no-console
+      console.log('Navigation');
+      // eslint-disable-next-line no-console
+      console.log(data);
+      return;
+    }
+
+    const requestData = JSON.stringify({
+      navigations: [data],
     });
+
+    if (typeof navigator.sendBeacon === 'function') {
+      navigator.sendBeacon('/internal/metrics/navigation', requestData);
+    } else {
+      await fetch('/internal/metrics/navigation', {
+        method: 'POST',
+        body: requestData,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
   });
 
   return <>{children}</>;
