6da799a7069fa3b6a1cf52b8abdb3287e5b87b6a
Chris Sauve <chrismsauve@gmail.com>
2023-02-20 14:15:46 -0500

    Save search to the URL

--- app/features/Search/Search.tsx
+++ app/features/Search/Search.tsx
@@ -1,5 +1,9 @@
-import {useState, useRef, useEffect} from 'react';
-import {usePerformanceNavigation} from '@quilted/quilt';
+import {useState, useRef, useEffect, useCallback} from 'react';
+import {
+  usePerformanceNavigation,
+  useCurrentUrl,
+  useNavigate,
+} from '@quilted/quilt';
 
 import {BlockStack, TextField, Action} from '@lemon/zest';
 
@@ -8,10 +12,32 @@ import {useQuery} from '~/shared/graphql';
 
 import searchQuery from './graphql/SearchQuery.graphql';
 
+const QUERY_PARAM = 'query';
+
 export default function Search() {
-  const [search, setSearch] = useState('');
-  const [committedSearch, setCommittedSearch] = useState(search);
-  const searchTimeout = useRef<null | number>(null);
+  const navigate = useNavigate();
+  const committedSearch = useCurrentUrl().searchParams.get(QUERY_PARAM) ?? '';
+  const [search, setSearch] = useState(committedSearch);
+
+  function updateCommittedSearch(search: string) {
+    navigate(
+      (currentUrl) => {
+        const newUrl = new URL(currentUrl);
+
+        if (search.trim().length === 0) {
+          newUrl.searchParams.delete(QUERY_PARAM);
+        } else {
+          newUrl.searchParams.set(QUERY_PARAM, search);
+        }
+        return newUrl;
+      },
+      {replace: true},
+    );
+  }
+
+  const handleSearchInput = useThrottledCallback(updateCommittedSearch, {
+    delay: 1_000,
+  });
 
   const {data, isLoading} = useQuery(searchQuery, {
     enabled: committedSearch.length > 0,
@@ -19,15 +45,6 @@ export default function Search() {
   });
 
   usePerformanceNavigation({state: isLoading ? 'loading' : 'complete'});
-
-  useEffect(
-    () => () => {
-      if (searchTimeout.current != null) {
-        clearTimeout(searchTimeout.current);
-      }
-    },
-    [],
-  );
 
   const series = data?.search.series ?? [];
 
@@ -37,16 +54,11 @@ export default function Search() {
         <TextField
           label="Search"
           value={search}
-          onChange={(value) => setSearch(value)}
-          onInput={(value) => {
-            if (searchTimeout.current != null) {
-              clearTimeout(searchTimeout.current);
-            }
-
-            searchTimeout.current = window.setTimeout(() => {
-              setCommittedSearch(value);
-            }, 1_000);
+          onChange={(value) => {
+            setSearch(value);
+            updateCommittedSearch(value);
           }}
+          onInput={handleSearchInput}
         />
         {series.length > 0 ? (
           <BlockStack>
@@ -59,5 +71,38 @@ export default function Search() {
         ) : null}
       </BlockStack>
     </Page>
+  );
+}
+
+function useThrottledCallback<T extends (...args: any[]) => any>(
+  callback: T,
+  {delay}: {delay: number},
+): (...args: Parameters<T>) => void {
+  const searchTimeout = useRef<null | number>(null);
+
+  const callbackRef = useRef(callback);
+  callbackRef.current = callback;
+
+  useEffect(
+    () => () => {
+      if (searchTimeout.current != null) {
+        clearTimeout(searchTimeout.current);
+      }
+    },
+    [],
+  );
+
+  return useCallback(
+    (...args) => {
+      if (searchTimeout.current != null) {
+        clearTimeout(searchTimeout.current);
+      }
+
+      searchTimeout.current = window.setTimeout(() => {
+        searchTimeout.current = null;
+        callbackRef.current(...args);
+      }, delay);
+    },
+    [delay],
   );
 }
