38eb3a3919ac4252f8f7ea6e03c0bd9ed21537d9
Chris Sauve <chrismsauve@gmail.com>
2023-06-06 01:54:12 -0400

    Pass-through `lines` property to SkeletonTextBlock

--- app/shared/clips/components/SkeletonTextBlock.tsx
+++ app/shared/clips/components/SkeletonTextBlock.tsx
@@ -3,7 +3,7 @@ import {createClipsComponent} from './shared.ts';
 
 export const SkeletonTextBlock = createClipsComponent(
   'ui-skeleton-text-block',
-  function SkeletonTextBlock({children}) {
-    return <UiSkeletonTextBlock>{children}</UiSkeletonTextBlock>;
+  function SkeletonTextBlock({children, lines}) {
+    return <UiSkeletonTextBlock lines={lines}>{children}</UiSkeletonTextBlock>;
   },
 );
