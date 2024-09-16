00d5363a8168da4c74f00b71d04253b7b9c896e8
Chris Sauve <chrismsauve@gmail.com>
2024-09-15 22:54:08 -0400

    Fix custom matcher type definitions :S

--- app/tests/render.ts
+++ app/tests/render.ts
@@ -1,6 +1,14 @@
-import '@quilted/quilt/testing';
+import {expect} from 'vitest';
+import {matchers, type CustomMatchers} from '@quilted/preact-testing/matchers';
 
 export {TestRouter} from '@quilted/quilt/navigation/testing';
 
 export * from './render/types.ts';
 export {renderApp} from './render/render.tsx';
+
+declare module 'vitest' {
+  interface Assertion<T = any> extends CustomMatchers<T> {}
+  interface AsymmetricMatchersContaining extends CustomMatchers {}
+}
+
+expect.extend(matchers);
